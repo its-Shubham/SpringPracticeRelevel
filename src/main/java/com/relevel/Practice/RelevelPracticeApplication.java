78e364d33a7f4fcbc8031931ab0b406381f8ba4b
SHUBHAM <shubhamsharmadob@gamil.com>
2023-02-11 19:57:43 +0530

    initial commit

---
+++ src/main/java/com/relevel/Practice/RelevelPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.relevel.Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelevelPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelevelPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relevel/Practice/RelevelPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelevelPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
